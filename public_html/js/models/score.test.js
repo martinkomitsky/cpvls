9a589a185caa8209579da968a315c3a8c1b8933b
Martin <martin00@yandex.ru>
2016-03-03 10:55:01 +0300

    cpvls

--- public_html/js/models/score.test.js
+++ public_html/js/models/score.test.js
@@ -6,31 +6,7 @@ define(function (require) {
         var ScoreModel = require('./score'),
             score = new ScoreModel();
 
-        QUnit.ok(score instanceof Backbone.Model, 'Zadikis');
+        QUnit.ok(score instanceof Backbone.Model);
 
-    });
-
-    QUnit.test("test sest", function () {
-        var //Backbone = require('backbone'),
-            //_ = require('underscore'),
-
-        	scores = require('collections/scores').toJSON(),
-        	scoresSorted = _.sortBy(scores, '-score');
-        console.info('scores', scores);
-        console.info('scoresSorted', scoresSorted);
-        var areArraysEqual = function(array1, array2) {
-            if (array1.length !== array2.length) {
-                return false;
-            }
-            for (var i = 0; i < array1.length; i++) {
-                if (array1[i].name !== array2[i].name && array1[i].score !== array2[i].score) {
-                	console.log("huynya")
-                    return false;
-                }
-            }
-            return true;
-        };
-
-        QUnit.ok(areArraysEqual(scores, scoresSorted), 'Returned array is sorted');
     });
 });
